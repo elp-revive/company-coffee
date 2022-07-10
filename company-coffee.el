e2cb4fd183aaec0bb2d81670e6a686a2704b1c40
Jen-Chieh Shen <jcs090218@gmail.com>
2022-07-11 00:12:39 +0800

    add src

--- company-coffee.el
+++ company-coffee.el
@@ -1,10 +1,11 @@
 ;;; company-coffee --- Emacs coffee completion
 
+;; Copyright (C) 2016, Noah Peart
+;; Copyright (C) 2022, Jen-Chieh Shen
+
 ;; Author: Noah Peart <noah.v.peart@gmail.com>
-;; URL: https://github.com/nverno/company-coffee
-;; Package-Requires: ((company "0.8.0") (cl-lib "0.5.0"))
-;; Copyright (C) 2016, Noah Peart, all rights reserved.
-;; Created: 24 August 2016
+;; URL: https://github.com/elp-revive/company-coffee
+;; Package-Requires: ((emacs "26.1") (company "0.8.12"))
 
 ;; This file is not part of GNU Emacs.
 ;;
@@ -29,8 +30,10 @@
 ;; Just adding the keywords for now.
 
 ;;; Code:
+
 (eval-when-compile
   (require 'cl-lib))
+
 (require 'company)
 
 (defgroup company-coffee nil
@@ -43,54 +46,50 @@
   :group 'company-coffee
   :type 'sexp)
 
-;; ------------------------------------------------------------
 ;; Internal
 (defvar company-keywords-alist)
 
-(eval-when-compile 
+(defvar company-coffee-compares
+  '(("is" . "===")
+    ("isnt" . "!==")
+    ("not" . "!")
+    ("and" . "&&")
+    ("or" . "||")
+    ("or=" . "||= (false, \"\", 0, null)")
+    ("?=" . "||= (only when null/undefined)")
+    ("true" . "true")
+    ("yes" . "true")
+    ("on" . "true")
+    ("false" . "false")
+    ("no" . "false")
+    ("off" . "false")))
 
- (defvar company-coffee-compares
-   '(("is" . "===")
-     ("isnt" . "!==")
-     ("not" . "!")
-     ("and" . "&&")
-     ("or" . "||")
-     ("or=" . "||= (false, \"\", 0, null)")
-     ("?=" . "||= (only when null/undefined)")
-     ("true" . "true")
-     ("yes" . "true")
-     ("on" . "true")
-     ("false" . "false")
-     ("no" . "false")
-     ("off" . "false")))
+(defvar company-coffee-math
+  '(("**" . "Math.pow")
+    ("//" . "Math.floor(a, b)")
+    ("%%" . "%")))
 
- (defvar company-coffee-math
-   '(("**" . "Math.pow")
-     ("//" . "Math.floor(a, b)")
-     ("%%" . "%")))
+;; "var" "function"
+(defvar company-coffee-words
+  '("__extends" "__hasProp" "alert" "await" "break" "by" "case" "catch"
+    "class" "const" "continue" "debugger" "default" "defer" "delete" "do"
+    "else" "enum" "export" "extends" "finally" "for" "if"
+    "import" "in" "instanceof" "let" "loop" "native" "new"
+    "of" "or" "own" "return" "super" "switch" "then"
+    "throw" "try" "typeof" "unless" "until" "void" "when" "while"
+    "with" "yield" "undefined" "null"
+    ))
 
- ;; "var" "function"
- (defvar company-coffee-words
-   '(
-     "__extends" "__hasProp" "alert" "await" "break" "by" "case" "catch"
-     "class" "const" "continue" "debugger" "default" "defer" "delete" "do"
-     "else" "enum" "export" "extends" "finally" "for" "if"
-     "import" "in" "instanceof" "let" "loop" "native" "new"
-     "of" "or" "own" "return" "super" "switch" "then"
-     "throw" "try" "typeof" "unless" "until" "void" "when" "while"
-     "with" "yield" "undefined" "null"
-     )))
- 
 (defvar company-coffee-keywords
   (eval-when-compile
     (sort
      (append
       (cl-loop for (k . v) in company-coffee-compares
-         do (put-text-property 0 1 'annot v k)
-         collect k)
+               do (put-text-property 0 1 'annot v k)
+               collect k)
       (cl-loop for (k . v) in company-coffee-math
-         do (put-text-property 0 1 'annot v k)
-         collect k)
+               do (put-text-property 0 1 'annot v k)
+               collect k)
       company-coffee-words)
      'string<)))
 
@@ -105,7 +104,6 @@
 (defun company-coffee-annotation (candidate)
   (or (get-text-property 0 'annot candidate) ""))
 
-
 ;;;###autoload
 (defun company-coffee (command &optional arg &rest _args)
   (interactive (list 'interactive))
@@ -116,17 +114,5 @@
     (sorted t)
     (candidates (company-coffee-candidates arg))))
 
-;; ;;;###autoload
-;; (defun company-coffee-add-keywords ()
-;;   "Add coffee keywords to `company-keywords-alist'."
-;;   (setcdr
-;;    (nthcdr (1- (length company-keywords-alist)) company-keywords-alist)
-;;    `(,(append '(coffee-mode) company-coffee-keywords))))
-
-;; ;;;###autoload
-;; (eval-after-load "coffee-mode"
-;;   '(company-coffee-add-keywords))
-
 (provide 'company-coffee)
-
 ;;; company-coffee.el ends here
